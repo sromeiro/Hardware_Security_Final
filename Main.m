e8a143ce1a1c7e143dc3fccf919efd96d00a909a
sromeiro <steven.romeiro@gmail.com>
2017-11-17 22:54:15 -0500

    MORE CODE

--- Main.m
+++ Main.m
@@ -10,6 +10,9 @@ fileNames = dir('Data/CSVData/*.csv');
 csvData = zeros(5003, 2, 10936);
 messageData = reshape(blanks(174976),10936,16); %creates 10936x16 char array
 cipherData = reshape(blanks(174976),10936,16); %creates 10936x16 char array
+messageBinary = zeros(16, 4, 10936); %Stores all messages in Binary
+cipherBinary = zeros(16, 4, 10936); %Stores all ciphers in Binary
+keyBinary = zeros(16, 4); %Stores the key in Binary
 
 %========================STORING OF CSV DATA==============================%
 % for i = 1:size(fileNames,1)
@@ -24,8 +27,16 @@ delimiters = strfind(fileNames(1).name, '_');
 %Store the key
 keyData = fileNames(1).name(36 : delimiters(7)-1);
 
+%For loop to store the key in Binary form
+for i = 1 : size(keyData, 2)
+    keyBinary(i,:) = hexToBinaryVector(keyData(1,i),4);
+end
+
 %For loop to store all of the messages, key and ciphertexts
-for i = 1:size(fileNames,1)
+for i = 1 : size(fileNames, 1)
     messageData(i, :) = fileNames(i).name(55 : delimiters(8)-1);
     cipherData(i, :) = fileNames(i).name(delimiters(8)+3 : 89);
 end
+
+%For loop to store the message and cipher in Binary form
+

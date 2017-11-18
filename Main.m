beb785c5c96de03fdc7963b9eed36c788b7380df
sromeiro <steven.romeiro@gmail.com>
2017-11-18 17:52:35 -0500

    added binary conversions

--- Main.m
+++ Main.m
@@ -10,9 +10,9 @@ fileNames = dir('Data/CSVData/*.csv');
 csvData = zeros(5003, 2, 10936);
 messageData = reshape(blanks(174976),10936,16); %creates 10936x16 char array
 cipherData = reshape(blanks(174976),10936,16); %creates 10936x16 char array
-messageBinary = zeros(16, 4, 10936); %Stores all messages in Binary
-cipherBinary = zeros(16, 4, 10936); %Stores all ciphers in Binary
-keyBinary = zeros(16, 4); %Stores the key in Binary
+messageBinary = zeros(10936, 64); %Stores all messages in Binary
+cipherBinary = zeros(10936, 64); %Stores all ciphers in Binary
+keyBinary = zeros(1, 64); %Stores the key in Binary
 
 %========================STORING OF CSV DATA==============================%
 % for i = 1:size(fileNames,1)
@@ -27,10 +27,10 @@ delimiters = strfind(fileNames(1).name, '_');
 %Store the key
 keyData = fileNames(1).name(36 : delimiters(7)-1);
 
-%For loop to store the key in Binary form
-for i = 1 : size(keyData, 2)
-    keyBinary(i,:) = hexToBinaryVector(keyData(1,i),4);
-end
+%For loop to store the key in Binary form array 16x4
+% for i = 1 : size(keyData, 2)
+%     keyBinary(i,:) = hexToBinaryVector(keyData(1,i),4);
+% end
 
 %For loop to store all of the messages, key and ciphertexts
 for i = 1 : size(fileNames, 1)
@@ -41,10 +41,11 @@ end
 %======================BINARY CONVERSION OF DATA==========================%
 %For loop to store the message and cipher in Binary form
 % for i = 1 : size(fileNames, 1)
-%     for j = 1 : size(messageData, 2)
-%         messageBinary(j, :, i) = hexToBinaryVector(messageData(i,j), 4);
-%         cipherBinary(j, :, i) = hexToBinaryVector(cipherData(i,j), 4);
-%     end
+%     messageBinary(i,:) =  hexToBinaryVector(messageData(i,:),64);
+%     cipherBinary(i,:) = hexToBinaryVector(cipherData(i,:),64);
 % end
+% 
+% %Store the key in Binary form
+% keyBinary = hexToBinaryVector(keyData,64);
 %=========================================================================%
 
